d6ec0fb7cfa0eb40d001236d38db1be3895d36bc
bgbotond <bgbotond@gmail.com>
2012-11-13 13:44:21 +0100

    change function name

--- samples/LicenseTest/src/LicenseTest.cpp
+++ samples/LicenseTest/src/LicenseTest.cpp
@@ -10,10 +10,7 @@ int main(int argc, char *argv[])
 {
 	License license;
 
-//	fs::path xmlData( getAssetPath( "license.xml" ));
-//	license.init( xmlData );
-
-	license.setKey( "public.pem" );
+	license.setKeyPath( "public.pem" );
 	license.setProduct( "TouchMovie" );
 	license.addServer( "http://www.mndl.hu/lcnc/licensehandler.php" );
 
